c70005de7a535f94e6e41135478762d1c8d892da
CyprianStream <163608322+CyprianStream@users.noreply.github.com>
2024-08-03 19:16:43 -0400

    Update todo-list.js

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -18,70 +18,43 @@ class ToDoList {
   static TEMPLATES = {
     TODOLIST: `modules/${this.ID}/templates/todo-list.hbs`
   }
-   /**
-   * A small helper function which leverages developer mode flags to gate debug logs.
-   * 
-   * @param {boolean} force - forces the log even if the debug flag is not on
-   * @param  {...any} args - what to log
-   */
-  static log(force, ...args) {  
-    const shouldLog = true;
-
-    if (shouldLog) {
-      console.log(this.ID, '|', ...args);
-    }
-  }
 }
 
 class ToDoListData {
   // all todos for all users
   static get allToDos() {
-    const allToDos = game.users.reduce((accumulator, user) => {
-      const userTodos = this.getToDosForUser(user.id);
-
-      return {
-        ...accumulator,
-        ...userTodos
-      }
-    }, {});
+    
+  };
 
     return allToDos;
   }
   // get all todos for a given user
-  static getToDosForUser(userId) {
-	  return game.users.get(userId)?.getFlag(ToDoList.FLAGS.TODOS);
+  static getToDosForUser(user.id) {
+	  return game.users.get(user.id)?.getFlag(ToDoList.ID,  ToDoList.FLAGS.TODOS);
   }
 
   // create a new todo for a given user
-  static createToDo(userId, toDoData) {
+  static createToDo(user.id, toDoData) {
+	  console.log("creating new todo")
 	  // generate a random id for this new ToDo and populate the userId
 	  const newToDo = {
-		  isDone: false,
-		  ...toDoData,
-		  id: foundry.utils.randomID(16),
-		  userId,
+		isDone: false,
+		...toDoData,
+		id: foundry.utils.randomID(16),
+		user.id,
 	  }
 	  // construct the update to insert the new todo
 	  const newToDos = {
 		  [newToDo.id]: newToDo
 	  }
 	  // update the database with the new todos
-	  return  game.users.get(userId)?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, newToDos);
+	  return  game.users.get(user.id)?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, newToDos);
   }
 
   // update a specific todo by id with the provided updateData
   static updateToDo(toDoId, updateData) {
-    const relevantToDo = this.allToDos[toDoId];
-
-    // construct the update to send
-    const update = {
-      [toDoId]: updateData
-    }
-
-    // update the database with the updated ToDo list
-    return game.users.get(relevantToDo.userId)?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, update);
+    
   }
-
   // delete a specific todo by id
   static deleteToDo(todoId) {}
 }
